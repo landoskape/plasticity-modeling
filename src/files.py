87336bcabe27df6fb9c5e6a46a180c6d548257ad
landoskape <andrewtylerlandau@gmail.com>
2025-03-12 09:09:46 +0000

    add typing and freezedry repo snapshot

--- src/files.py
+++ src/files.py
@@ -1,19 +1,20 @@
 from pathlib import Path
+from freezedry import freezedry
 
 
-def root_dir():
+def root_dir() -> Path:
     return Path(__file__).parent.parent
 
 
-def config_dir():
+def config_dir() -> Path:
     return root_dir() / "configs"
 
 
-def get_config_path(filename: str):
+def get_config_path(filename: str) -> Path:
     return config_dir() / filename
 
 
-def data_dir(folder: str | None = None):
+def data_dir(folder: str | None = None) -> Path:
     directory = root_dir() / "data"
     if folder is not None:
         directory = directory / folder
@@ -22,7 +23,7 @@ def data_dir(folder: str | None = None):
     return directory
 
 
-def results_dir(folder: str | None = None):
+def results_dir(folder: str | None = None) -> Path:
     directory = root_dir() / "results"
     if folder is not None:
         directory = directory / folder
@@ -31,7 +32,7 @@ def results_dir(folder: str | None = None):
     return directory
 
 
-def get_figure_dir(folder=None):
+def get_figure_dir(folder: str | None = None) -> Path:
     figure_dir = root_dir() / "media" / "figures"
     if folder:
         figure_dir = figure_dir / folder
@@ -40,5 +41,9 @@ def get_figure_dir(folder=None):
     return figure_dir
 
 
-def get_natural_images_dir():
+def get_natural_images_dir() -> Path:
     return root_dir() / "data" / "IMAGES.mat"
+
+
+def save_repo_snapshot(output_path: Path, verbose: bool = True) -> None:
+    freezedry(directory_path=root_dir(), output_path=output_path, ignore_git=True, use_gitignore=True, verbose=verbose)
