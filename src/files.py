eb860a040b658a0843c05fca21d8e29fc2010054
landoskape <andrewtylerlandau@gmail.com>
2025-03-12 08:10:07 +0000

    prepping some target directories

--- src/files.py
+++ src/files.py
@@ -5,6 +5,32 @@ def root_dir():
     return Path(__file__).parent.parent
 
 
+def config_dir():
+    return root_dir() / "configs"
+
+
+def get_config_path(filename: str):
+    return config_dir() / filename
+
+
+def data_dir(folder: str | None = None):
+    directory = root_dir() / "data"
+    if folder is not None:
+        directory = directory / folder
+    if not directory.exists():
+        directory.mkdir(parents=True, exist_ok=True)
+    return directory
+
+
+def results_dir(folder: str | None = None):
+    directory = root_dir() / "results"
+    if folder is not None:
+        directory = directory / folder
+    if not directory.exists():
+        directory.mkdir(parents=True, exist_ok=True)
+    return directory
+
+
 def get_figure_dir(folder=None):
     figure_dir = root_dir() / "media" / "figures"
     if folder:
