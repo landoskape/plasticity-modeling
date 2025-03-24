508e5c55a3af459fa02c730f280246d25d57ef44
Andrew Landau <andrewtylerlandau@gmail.com>
2025-03-24 12:40:56 +0000

    save schematic

--- scripts/build_schematics.py
+++ scripts/build_schematics.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from src.schematics import Neuron
+from src.files import get_figure_dir
+from src.plotting import save_figure
 
 
 if __name__ == "__main__":
@@ -9,6 +11,12 @@ if __name__ == "__main__":
     ax.set_xlim(-2.5, 2.5)
     ax.set_ylim(-1, 6)
     ax.set_aspect("equal")
-    ax.set_axisbelow(True)
-    plt.grid(True)
-    plt.show()
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+
+    figure_path = get_figure_dir("schematic") / "dendrite_branch_schematic"
+    save_figure(fig, figure_path)
