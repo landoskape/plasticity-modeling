3c984dff7c7c03be138dda9faf6f78fb8287a806
landoskape <andrewtylerlandau@gmail.com>
2025-04-17 10:52:04 +0100

    No need for old code anymore

--- old_directories/circularFeature1/changeStimulus.m
+++
@@ -1,8 +0,0 @@
-function stimulus = changeStimulus(stimulus, dT)
-
-nudgeFeature = dT*stimulus.nudgeRate .* stimulus.dNudge.*randn(1,stimulus.numFeatures);
-jumpFeature = stimulus.dJump .* randn(1,stimulus.numFeatures);
-jumpProbability =  rand(1,stimulus.numFeatures)<(dT*stimulus.jumpRate);
-
-stimulus.value = stimulus.value + (nudgeFeature + jumpFeature.*jumpProbability);
-  
